c8e5818142085d3f456e8159d131b54d4833a24b
Denesterio <mrdnsong@gmail.com>
2021-06-30 21:57:27 +0300

    Add graph component

--- src/App.vue
+++ src/App.vue
@@ -169,49 +169,10 @@
         </dl>
         <hr class="w-full border-t border-gray-600 my-4" />
       </template>
-      <section v-if="selectedTicker" class="relative">
-        <h3 class="text-lg leading-6 font-medium text-gray-900 my-8">
-          {{ selectedTicker.name }} - USD
-        </h3>
-        <div
-          ref="graph"
-          class="flex items-end border-gray-600 border-b border-l h-64"
-        >
-          <div
-            v-for="(bar, i) in normalizedGraph"
-            :key="i"
-            :style="{ height: `${bar}%`, width: `${this.graphBarWidth}px` }"
-            class="bg-purple-800 border"
-          ></div>
-        </div>
-        <button
-          @click="selectedTicker = null"
-          type="button"
-          class="absolute top-0 right-0"
-        >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            xmlns:xlink="http://www.w3.org/1999/xlink"
-            xmlns:svgjs="http://svgjs.com/svgjs"
-            version="1.1"
-            width="30"
-            height="30"
-            x="0"
-            y="0"
-            viewBox="0 0 511.76 511.76"
-            style="enable-background: new 0 0 512 512"
-            xml:space="preserve"
-          >
-            <g>
-              <path
-                d="M436.896,74.869c-99.84-99.819-262.208-99.819-362.048,0c-99.797,99.819-99.797,262.229,0,362.048    c49.92,49.899,115.477,74.837,181.035,74.837s131.093-24.939,181.013-74.837C536.715,337.099,536.715,174.688,436.896,74.869z     M361.461,331.317c8.341,8.341,8.341,21.824,0,30.165c-4.16,4.16-9.621,6.251-15.083,6.251c-5.461,0-10.923-2.091-15.083-6.251    l-75.413-75.435l-75.392,75.413c-4.181,4.16-9.643,6.251-15.083,6.251c-5.461,0-10.923-2.091-15.083-6.251    c-8.341-8.341-8.341-21.845,0-30.165l75.392-75.413l-75.413-75.413c-8.341-8.341-8.341-21.845,0-30.165    c8.32-8.341,21.824-8.341,30.165,0l75.413,75.413l75.413-75.413c8.341-8.341,21.824-8.341,30.165,0    c8.341,8.32,8.341,21.824,0,30.165l-75.413,75.413L361.461,331.317z"
-                fill="#718096"
-                data-original="#000000"
-              ></path>
-            </g>
-          </svg>
-        </button>
-      </section>
+      <graph-component
+        v-bind="selectedTicker"
+        @deselect-ticker="deselectTicker"
+      />
     </div>
   </div>
 </template>
@@ -219,12 +180,14 @@
 <script>
 import { subscribeToTicker, unsubscribeFromTicker } from "./api.js";
 import AddTickerComponent from "./components/AddTickerComponent.vue";
+import GraphComponent from "./components/GraphComponent.vue";
 
 export default {
   name: "App",
 
   components: {
     AddTickerComponent,
+    GraphComponent,
   },
 
   data() {
@@ -233,13 +196,9 @@ export default {
 
       tickers: [],
       selectedTicker: null,
-      graph: [],
-      graphBarWidth: 28,
-      maxGraphLength: 1,
 
       isTickerNotValid: false,
       maxTickersQuantity: 18,
-      currencies: [],
 
       page: 1,
     };
@@ -265,14 +224,6 @@ export default {
     }
   },
 
-  mounted() {
-    window.addEventListener("resize", this.changeGraphElementWidth);
-  },
-
-  beforeUnmount() {
-    window.removeEventListener("resize", this.changeGraphElementWidth);
-  },
-
   computed: {
     startIndex() {
       return (this.page - 1) * 6;
@@ -294,17 +245,6 @@ export default {
 
     hasNextPage() {
       return this.filteredTickers.length > this.endIndex;
-    },
-
-    normalizedGraph() {
-      const max = Math.max(...this.graph);
-      const min = Math.min(...this.graph);
-
-      if (max === min) {
-        return this.graph.map(() => 50);
-      }
-
-      return this.graph.map((price) => 5 + ((price - min) * 95) / (max - min));
     },
 
     pageStateOptions() {
@@ -329,26 +269,6 @@ export default {
         currentTicker.valid = false;
         return;
       }
-      if (tickerName === this.selectedTicker?.name) {
-        this.graph.push(price);
-      }
-      this.normalizeGraphElementWidth();
-    },
-
-    calculateMaxGraphLength() {
-      if (!this.$refs.graph) return;
-      this.maxGraphLength = this.$refs.graph.clientWidth / this.graphBarWidth;
-    },
-
-    normalizeGraphElementWidth() {
-      if (this.graph.length > this.maxGraphLength) {
-        this.graph = this.graph.slice(this.graph.length - this.maxGraphLength);
-      }
-    },
-
-    changeGraphElementWidth() {
-      this.calculateMaxGraphLength();
-      this.normalizeGraphElementWidth();
     },
 
     formatPrice(price) {
@@ -397,6 +317,10 @@ export default {
       this.selectedTicker = ticker;
     },
 
+    deselectTicker() {
+      this.selectedTicker = null;
+    },
+
     clearValidation() {
       this.isTickerNotValid = false;
     },
@@ -405,11 +329,6 @@ export default {
   watch: {
     tickers() {
       localStorage.setItem("cryptovue-list", JSON.stringify(this.tickers));
-    },
-
-    selectedTicker() {
-      this.graph = [];
-      this.$nextTick().then(this.calculateMaxGraphLength);
     },
 
     filter() {
